758f5dd1a7ef40af22772571dd9c4948d355a515
Sourav2003-03 <mukherjeesourav289@gmail.com>
2023-11-09 10:01:32 +0530

    Add files via upload

---
+++ Includes/GoogleMap.php
@@ -0,0 +1,11 @@
+<?php include "./AdditionalPHP/checkAccess.php"; ?>
+
+<div class="map">
+    <div class="subtitle">
+        <h2>LOCATE US</h2>
+    </div>
+
+    <div class="google-map">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3505.6334665030113!2d77.22634697522189!3d28.520673489152045!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce18454c9f46b%3A0x4c4d7686133c3393!2sJuneja%20Bakers%20%26%20sweets!5e0!3m2!1sen!2sin!4v1698298393423!5m2!1sen!2sin" width="1525" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+</div>
